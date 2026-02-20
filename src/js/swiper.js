a82c787e172c452a3682398ecfbf3db96637d8dc
JustFun <paw93@ukr.net>
2026-02-20 10:04:51 +0200

    ++

---
+++ src/js/swiper.js
@@ -0,0 +1,27 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+// import 'css-star-rating/dist/css-star-rating.min.css';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  spaceBetween: 20,
+  loop: true,
+  speed: 500,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+});
