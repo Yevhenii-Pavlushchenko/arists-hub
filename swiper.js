5b804756eac3886ca7f89375f3252e9de986432d
Yevhenii-Pavlushchenko <103812722+Yevhenii-Pavlushchenko@users.noreply.github.com>
2026-02-25 13:07:04 +0000

    Deploying to gh-pages from @ Yevhenii-Pavlushchenko/arists-hub@a268acd27ee247418836ce1eb7281fd1bb4fbba4 🚀

--- swiper.js
+++ swiper.js
@@ -1,2 +1,2 @@
-import"./assets/styles-1m9JF8WR.js";import{S as e,N as i,P as t}from"./assets/vendor-BYtaHlT3.js";new e(".swiper",{spaceBetween:20,loop:!0,speed:500,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",type:"bullets",clickable:!0},modules:[i,t]});//!==============STARS============
+import{S as e,N as i,P as t}from"./assets/pagination-Y5Kh12Jz.js";new e(".swiper",{spaceBetween:20,loop:!0,speed:500,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",type:"bullets",clickable:!0},modules:[i,t]});//!==============STARS============
 //# sourceMappingURL=swiper.js.map
